f5e411460875badd6fcb018f688fc81684cd8d57
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2024-06-08 14:46:54 +0530

    demo user page

---
+++ src/app/user/page.tsx
@@ -0,0 +1,19 @@
+"use server"
+
+import { getServerSession } from "next-auth"
+
+async function getUser() {
+  const session = await getServerSession();
+  return session;
+}
+
+export default async function Home() {
+  const session = await getUser();
+
+  return (
+    <div>
+        HELLOo
+      {JSON.stringify(session?.user?.name)}
+    </div>
+  );
+}
