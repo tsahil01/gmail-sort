8ae15a08c5177d89fb19491484a1cb7def18a04c
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2024-06-08 14:47:27 +0530

    moved NEXTAUTH_CONFIG to lib

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,51 +1,6 @@
+import { NEXTAUTH_CONFIG } from "@/lib/auth";
 import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
 
-const handler = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-      authorization: {
-        params: {
-            scope: 'openid profile email https://www.googleapis.com/auth/gmail.readonly'
-        }
-      }
-    })
-  ],
-  secret: process.env.SECRET,
-
-  callbacks: {
-    async signIn({ user, account, profile }) {
-      console.log("signIn", user, account, profile);
-      return true;
-    },
-
-    async jwt({ token, account }) {
-        // console.log("jwt", token, account);
-        
-        if(account) {
-            token.accessToken = account?.access_token;
-        }
-        console.log("jwt", token, account);
-        return token;
-      },
-
-    async session({ session, token, user }) {
-    //   console.log("session", session, token, user);
-    
-      //   @ts-ignore
-      session.accessToken = token.accessToken;
-        console.log("session", session, token, user);
-      return session;
-    },
-    
-    async redirect({ url, baseUrl }) {
-        console.log("redirect", url, baseUrl);
-        return baseUrl;
-      },
-
-  }
-});
+const handler = NextAuth(NEXTAUTH_CONFIG);
 
 export { handler as GET, handler as POST };
