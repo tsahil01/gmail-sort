dbde6ca4a964438ba5aff84b650771db4392e0f3
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2024-06-09 17:59:54 +0530

    fix

--- src/components/ApiKeyComponent.tsx
+++ src/components/ApiKeyComponent.tsx
@@ -25,7 +25,7 @@ export default function ApiKeyComponent() {
                     <CardTitle>API Key</CardTitle>
                 </CardHeader>
                 <CardContent className="space-y-4">
-                    <div>If you don&#39;t have an API key, just select provider as Gemini and proceed.</div>
+                    <div>If you dont have an API key, just select provider as Gemini and proceed.</div>
                     <div className="space-y-2">
                         <form>
                             <div className="grid w-full items-center gap-4">
